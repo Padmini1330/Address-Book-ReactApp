eb5fffb7c522f8af8bc2c270381da3511ec9b464
padmini v <padmini.v@perfios.com>
2021-11-16 00:37:03 +0530

    ADDED: routers to route to different page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import AddressBookForm from './components/address-book-form/addressbookform.jsx';
+import AddressBookHome from './components/address-book-home/addressbookhome.jsx';
+import { Route,Routes,BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>  
+    <Routes>
+      <Route exact path="/form" element={<AddressBookForm />}/>
+      <Route exact path="/" element={<AddressBookHome />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
