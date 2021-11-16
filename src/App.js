6fecdb834669c72d45fba137d91ffb8d6d338257
padmini v <padmini.v@perfios.com>
2021-11-16 22:48:31 +0530

    ADDED: routers to route to different page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import AddressBookForm from './components/address-book-form/addressbookform.jsx'
 import AddressBookHome from './components/address-book-home/addressbookhome.jsx';
 import { Route,Routes,BrowserRouter } from 'react-router-dom';
 
-function App() {
+function App(){
   return (
     <BrowserRouter>  
     <Routes>
       <Route exact path="/form" element={<AddressBookForm />}/>
       <Route exact path="/" element={<AddressBookHome />}/>
+      <Route exact path="/form/:id" element={<AddressBookForm />}/>
     </Routes>
     </BrowserRouter>
   );
