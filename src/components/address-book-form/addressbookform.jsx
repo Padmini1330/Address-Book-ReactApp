ffaba9e95d68e0b489dbe934a2f3832697593b46
padmini v <padmini.v@perfios.com>
2021-11-17 00:22:04 +0530

    MODIFIED: Ability to run the front end with spring api calls

--- src/components/address-book-form/addressbookform.jsx
+++ src/components/address-book-form/addressbookform.jsx
@@ -8,7 +8,6 @@ import AddressBookService from '../../services/address-book-service';
 const AddressBookForm = (props) => {
 
     const { id } = useParams()
-    //alert(id)
 
     let initialValue = {
         name: '',
@@ -38,47 +37,48 @@ const AddressBookForm = (props) => {
       }
       
       const validData=async()=>{
-          let isError=false;
-          let error={
-              name:'',
-              phoneNumber:'',
-              address:'',
-              city:'',
-              state:'',
-              zip:''
-          }
-          if(formValue.name.length <1)
-          {
-              error.name='Name is required field'
-              isError=true;
-          }
-          if(formValue.phoneNumber.length <1)
-          {
-              error.name='phoneNumber is required field'
-              isError=true;
-          }
-          if(formValue.address.length <1)
-          {
-              error.name='Address is required field'
-              isError=true;
-          }
-          if(formValue.city.length <1)
-          {
-              error.name='City is required field'
-              isError=true;
-          }
-          if(formValue.state.length <1)
-          {
-              error.name='State is required field'
-              isError=true;
-          }
-          if(formValue.zip.length <1)
-          {
-              error.name='Zip is required field'
-              isError=true;
-          }
-          await setForm({...formValue,error:error})
-          return isError;
+        let isError = false;
+        let error = {
+            name: '',
+            phoneNumber:'',
+            address: '',
+            city: '',
+            state: '',
+            zip: ''
+
+        }
+        const namePattern = /^[A-Z]{1}[A-Za-z]{2,}([\s]?([a-zA-Z]+))*$/
+        if (!namePattern.test(formValue.name) || formValue.name.length < 1 ) {
+            error.name = 'Invalid Name'
+            isError = true;
+        }
+        const phoneNumberPattern = /^[+]?([0-9]{2})?[789]{1}[0-9]{9}$/
+        if (!phoneNumberPattern.test(formValue.phoneNumber) || formValue.phoneNumber.length < 1) {
+            error.phoneNumber = 'Invalid PhoneNumber'
+            isError = true;
+        }
+        const addressPattern = /^[a-zA-Z0-9]{3,}([\s]?[a-zA-Z0-9]{3,})*$/
+        if (!addressPattern.test(formValue.address) || formValue.address.length < 1 ) {
+            error.address = 'Invalid Address'
+            isError = true;
+        }
+        const zipPattern = /^[0-9]{3}[\s]?[0-9]{3}$/
+        if (!zipPattern.test(formValue.zip) || formValue.zip.length < 1 ) {
+            error.zip = 'Invalid ZipCode'
+            isError = true;
+        }
+
+        if (formValue.city.length < 1) {
+            error.city = 'City is required'
+            isError = true;
+        }
+        if (formValue.state.length < 1) {
+            error.state = 'State is required'
+            isError = true;
+        }
+
+        await setForm({ ...formValue, error: error })
+        return isError;
       }
       
 
@@ -89,12 +89,12 @@ const AddressBookForm = (props) => {
             addressBookService.getContact(id).then(contact => {
                 setForm({
                     ...formValue,
-                    name: contact.data.name,
-                    phoneNumber: contact.data.phoneNumber,
-                    address: contact.data.address,
-                    city: contact.data.city,
-                    state: contact.data.state,
-                    zip: contact.data.zip
+                    name: contact.data.data.firstName,
+                    phoneNumber: contact.data.data.phoneNumber,
+                    address: contact.data.data.lastName,
+                    city: contact.data.data.city,
+                    state: contact.data.data.state,
+                    zip: contact.data.data.zipCode
                 });
             }).catch(error => {
                 console.log(error)
@@ -105,7 +105,6 @@ const AddressBookForm = (props) => {
 
       const save = async(event)=>{
         event.preventDefault();
-        alert("inside save")
       
         if(await validData())
         {
@@ -117,24 +116,24 @@ const AddressBookForm = (props) => {
         {
         let object = {
 
-                name:formValue.name,
+                firstName:formValue.name,
                 phoneNumber:formValue.phoneNumber,
-                address:formValue.address,
+                lastName:formValue.address,
                 city:formValue.city,
                 state:formValue.state,
-                zip:formValue.zip,
+                zipCode:formValue.zip,
             }
             addressBookService.updateContact(object,id);
         }
         else
         {
                 let object = {
-                    name:formValue.name,
+                    firstName:formValue.name,
                     phoneNumber:formValue.phoneNumber,
-                    address:formValue.address,
+                    lastName:formValue.address,
                     city:formValue.city,
                     state:formValue.state,
-                    zip:formValue.zip,
+                    zipCode:formValue.zip,
                 }
 
                 addressBookService.addContact(object).then(data=>{
@@ -180,22 +179,23 @@ const AddressBookForm = (props) => {
                         <div className="row-content">
                             <label htmlFor="name" className="label text">Full Name</label>
                             <input type="text" className="input" id="name" name="name" value={formValue.name} onChange={changeValue} placeholder="Your name.."required/>
-                            
+                            <div className="error" id="name-error">{formValue.error.name}</div>
                         </div>
                         <div className="row-content">
                             <label htmlFor="phone-number" className="label text">Phone Number</label>
                             <input type="tel" className="input" id="phoneNumber" name="phoneNumber" value={formValue.phoneNumber} onChange={changeValue} required/>
-                            
+                            <div className="error" id="phoneNumber-error">{formValue.error.phoneNumber}</div>
                         </div>
                         <div className="row-content">
                             <label htmlFor="address" className="text">Address</label>
                             <textarea name="address" id="address" cols="50" rows="6" value={formValue.address} onChange={changeValue} required></textarea>
-    
+                            <div className="error" id="address-error">{formValue.error.address}</div>
                         </div>
                         <div className="row-content inner-rows">
                             <div className="columns-content">
                                 <label htmlFor="city" className="label text">City</label>
-                                <select name="city" id="city" className="input" onChange={changeValue}>
+                                <select name="city" id="city" className="input" onChange={changeValue}
+                                value={formValue.city}>
                                     <option value="0">Select City</option>
                                     <option value="Bangalore">Bangalore</option>
                                     <option value="Mumbai">Mumbai</option>
@@ -203,10 +203,12 @@ const AddressBookForm = (props) => {
                                     <option value="Pune">Pune</option>
                                     <option value="Mysore">Mysore</option>
                                 </select>
+                                <div className="error" id="city-error">{formValue.error.city}</div>
                             </div>
                             <div className="columns-content">
                                 <label htmlFor="state" className="text label">State</label>
-                                <select name="state" id="state" className="input" onChange={changeValue}>
+                                <select name="state" id="state" className="input" onChange={changeValue}
+                                value={formValue.state}>
                                     <option value="0">Select State</option>
                                     <option value="Karnataka">Karnataka</option>
                                     <option value="Andhra Pradesh">Andhra Pradesh</option>
@@ -217,11 +219,13 @@ const AddressBookForm = (props) => {
                                     <option value="Uttarakhand">Uttarakhand</option>
                                     <option value="West Bengal">West Bengal</option>
                                 </select>
+                                <div className="error" id="state-error">{formValue.error.state}</div>
                             </div>
                             <div className="columns-content zip">
                                 <label htmlFor="zip" className="text label">Zip Code</label>
                                 <input type="tel" className="input" id="zip" name="zip"  
                                 value={formValue.zip} onChange={changeValue} required/>
+                                <div className="error" id="zip-error">{formValue.error.zip}</div>
                                 
                             </div>
                         </div>
